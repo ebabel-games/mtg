8858b6ea88dfb4f1c109fb118e479efb41997001
Thomas Amar <hello@ebabel.eu>
2022-07-29 17:49:44 +0200

    Support hard-coded option values.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Dropdown from './components/Dropdown';
 function App() {
   return (
     <>
-      <Dropdown label="Sets" local="/data/2022-07-29/api.magicthegathering.io/v1/sets.json" localName="sets" />
-
+      <Dropdown label="Max number of cards" options={[5, 10, 20, 40]} localName="pageSize" />
       <Dropdown label="Types" local="/data/2022-07-29/api.magicthegathering.io/v1/types.json" localName="types" />
+      <Dropdown label="Formats" local="/data/2022-07-29/api.magicthegathering.io/v1/formats.json" localName="formats"  />
+      <Dropdown label="Sets" local="/data/2022-07-29/api.magicthegathering.io/v1/sets.json" localName="sets" />
     </>
   );
 }
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,44 +5,63 @@ function Dropdown(props) {
   const [status, setStatus] = useState('');
   const [options, setOptions] = useState([]);
 
+  function convertDataToOptions(data) {
+    let key = 0;
+    const _options = data
+      .map(s => {
+        key += 1;
+        const _key = `${props.localName}_${key}`;
+
+        // Array element is an object.
+        if (s.code && s.name) {
+          return { key: _key, code: s.code, name: s.name };
+        }
+
+        // Array element is a simple value.
+        return { key: _key, code: s, name: s };
+    });
+
+    return _options;
+  }
+
   useEffect(() => {
     setStatus('Loading');
+
+    // Hard-coded options passed as props.
+    if (props.options) {
+      setOptions(convertDataToOptions(props.options));
+      setStatus('Success');
+      return;
+    }
+
+    // Get options dynamically if not available in props.
     fetch(props.local)
       .then(res => res.json())
       .then(res => {
         if (res && res[props.localName]) {
-          let key = 0;
-          const _options = res[props.localName]
-            .map(s => {
-              key += 1;
-              const _key = `${props.localName}_${key}`;
-              if (s.code && s.name) {
-                return { key: _key, code: s.code, name: s.name };
-              }
-
-              return { key: _key, code: s, name: s };
-          });
-          setOptions(_options);
+          setOptions(convertDataToOptions(res[props.localName]));
+        } else {
+          throw new Error('Missing data');
         }
       })
       .then(() => setStatus('Success'))
       .catch(() => setStatus('Error'));
   }, []); // Second parameter of useEffect must be [] otherwise an infinite loop is created.
 
-  return (
-    <>
-      {status === 'Loading' && <p className="placeholder-dropdown" />}
-      {status === 'Error' && <p className="error">Something went wrong</p>}
-      {status === 'Success' && (
-        <label>
-          <p>{props.label}</p>
-          <select>
-            {options.map(o => <option key={o.key} value={o.code}>{o.name}</option>)}
-          </select>
-        </label>
-      )}
-    </>
-  );
+    return (
+      <>
+        {status === 'Loading' && <p className="placeholder-dropdown" />}
+        {status === 'Error' && <p className="error">Something went wrong</p>}
+        {status === 'Success' && (
+          <label>
+            <p>{props.label}</p>
+            <select>
+              {options.map(o => <option key={o.key} value={o.code}>{o.name}</option>)}
+            </select>
+          </label>
+        )}
+      </>
+    );
 }
 
 export default Dropdown;
